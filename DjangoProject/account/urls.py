c3849a13dde2f752e106d7bdba88b631682f0c9f
youngkwon02 <youngkwon02@naver.com>
2021-10-04 11:21:47 +0900

    feat: url connection

--- DjangoProject/account/urls.py
+++ DjangoProject/account/urls.py
@@ -1,8 +1,8 @@
+from . import views
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('account.urls')),
+  path("verifyUniv/", views.verify_univ),
 ]
