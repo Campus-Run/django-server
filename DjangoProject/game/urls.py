7e9b39db2e6f2238a344e8e5fd8f744dc32e54a4
youngkwon02 <youngkwon02@naver.com>
2021-11-25 05:19:17 +0900

    refactor: invitation_by_id api res to uppercase

--- DjangoProject/game/urls.py
+++ DjangoProject/game/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("api/create-room", views.create_room),
     path("api/check-room-full", views.check_room_full),
     path("api/create-room", views.create_room),
-    path("api/send-invite", views.send_invite)
+    path("api/send-invite", views.send_invite),
+    path("api/invitation-by-id", views.invitation_by_id)
 ]
--- DjangoProject/game/views.py
+++ DjangoProject/game/views.py
@@ -79,7 +79,35 @@ def send_invite(request):
                 Invitation.objects.create(receiver=receiver, url=url, title=title, creater=creater_obj, home_univ=owner_univ_obj,
                                           away_univ=opponent_univ_obj, is_home=False, max_join=max_join)
 
-            return JsonResponse(status=200, data={'status': 200, 'message': "성공적으로 Game Room을 생성하였습니다."})
+            return JsonResponse(status=200, data={'status': 200, 'message': "성공적으로 초대장을 전송하였습니다."})
+        except:
+            return JsonResponse(status=500, data={'status': 500, 'message': "잘못된 입력 데이터입니다."})
+    return_data = {'status': 500, 'message': "Request Method가 잘못되었습니다."}
+    print(return_data)
+    return JsonResponse(status=500, data=return_data)
+
+
+def invitation_by_id(request):
+    if request.method == 'GET':
+        try:
+            kakao_id = request.GET['kakaoId']
+            receiver_obj = user.objects.filter(kakao_id=kakao_id)[0]
+            inv_qs = Invitation.objects.filter(receiver=receiver_obj)
+            result = []
+            for qs in inv_qs:
+                result.append({
+                    'title': qs.title,
+                    'creater': qs.creater.kakao_name,
+                    'url': qs.url,
+                    'receiver': qs.receiver.kakao_name,
+                    'homeUniv': qs.home_univ.name,
+                    'awayUniv': qs.away_univ.name,
+                    'isHome': qs.is_home,
+                    'maxJoin': qs.max_join,
+                    'isRead': qs.is_read,
+                    'createDate': qs.created_at,
+                })
+            return JsonResponse(status=200, data={'status': 200, 'message': "성공적으로 Game Room을 생성하였습니다.", "data": result})
         except:
             return JsonResponse(status=500, data={'status': 500, 'message': "잘못된 입력 데이터입니다."})
     return_data = {'status': 500, 'message': "Request Method가 잘못되었습니다."}
