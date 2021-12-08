980cd247106285a9845986271a125c2d5a71558e
youngkwon02 <youngkwon02@naver.com>
2021-12-09 02:30:09 +0900

    refactor: game_end_check api res format

--- DjangoProject/game/views.py
+++ DjangoProject/game/views.py
@@ -670,8 +670,8 @@ def game_end_check(request):
         
         for rec in record_obj:
             if rec.end != None:
-                return JsonResponse(status=200, data={'status': 'end', 'winner': rec.user.kakao_name})
-        return JsonResponse(status=200, data={'status': 'playing'})
+                return JsonResponse(status=200, data={'status': 200, 'gameStatus': 'end' 'winner': rec.user.kakao_name})
+        return JsonResponse(status=200, data={'status': 200, 'gameStatus': 'playing'})
 
     return_data = {'status': 500, 'message': "Request Method가 잘못되었습니다."}
     return JsonResponse(status=500, data=return_data)
